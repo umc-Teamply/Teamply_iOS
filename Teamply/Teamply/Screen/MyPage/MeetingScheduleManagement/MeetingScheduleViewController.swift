774cf6aefbec8ee1c81080cef21ff8d479330919
orzr <tjsdkfk77@naver.com>
2023-01-29 22:03:51 +0900

    [Feat] UIComponent 배치 #13

---
+++ Teamply/Teamply/Screen/MyPage/MeetingScheduleManagement/MeetingScheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MeetingScheduleViewController.swift
+//  Teamply
+//
+//  Created by 아라 on 2023/01/29.
+//
+
+import UIKit
+
+class MeetingScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
