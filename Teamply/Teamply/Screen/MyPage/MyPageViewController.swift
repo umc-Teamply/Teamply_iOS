68fdd0dd927891a7ca5969ed8729c1ea2880067c
orzr <tjsdkfk77@naver.com>
2023-01-29 14:40:09 +0900

    [Feat] 컴포넌트 연결 #11

--- Teamply/Teamply/Screen/MyPage/MyPageViewController.swift
+++ Teamply/Teamply/Screen/MyPage/MyPageViewController.swift
@@ -8,8 +8,27 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    // MARK: - IBOultet
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var scheduleLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var termsOfUseLabel: UILabel!
+    @IBOutlet weak var privacyPolicyLabel: UILabel!
+    @IBOutlet weak var helpLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Method
+    
+    // MARK: - IBAction
+    
+    // MARK: - Gesture
 }
