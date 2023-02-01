dfa67f9141f5b576b02625144b7d239610e8fb3d
orzr <tjsdkfk77@naver.com>
2023-02-01 22:17:05 +0900

    [Feat] tableViewCell에 TableView 추가 #17

--- Teamply/Teamply/Screen/Box/CompletedViewController.swift
+++ Teamply/Teamply/Screen/Box/CompletedViewController.swift
@@ -9,18 +9,23 @@ import UIKit
 
 class CompletedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: - IBOutlet
-    @IBOutlet weak var semesterProjectTableView: UITableView!
+    @IBOutlet weak var semesterTableView: UITableView!
     
     // MARK: - Properties
     let semesterList: [String] = ["2022년 1학기", "2021년 2학기"]
-    // MARK: - LifeCycle
     let semesterCell = "SemesterCell"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        semesterProjectTableView.delegate = self
-        semesterProjectTableView.dataSource = self
-     
-    }
+    let projectCell = "ProjectCell"
+    let projectLists: [[[String]]] =
+        [
+            [
+                ["현대 디자인의 이해", "현대 디자인 조사 및 발표", "2022.10.15"],
+                ["부모와 가정의 이해", "다양한 가정의 모습 탐구", "2022.09.12"]
+            ],[
+                ["기초 GUI 디자인", "어플리케이션 클론", "2022.04.04"]
+            ]
+        ]
+    // MARK: - LifeCycle
+    
     
     // MARK: - Method
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,10 +34,17 @@ class CompletedViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: semesterCell, for: indexPath) as! SemesterProjectTableViewCell
+        
         cell.semesterLabel.text = semesterList[indexPath.row]
+        cell.projectTableView.dequeueReusableCell(withIdentifier: projectCell, for: indexPath) as! ProjectTableViewCell
         return cell
         
     }
-  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        semesterTableView.delegate = self
+        semesterTableView.dataSource = self
+    }
 
 }
