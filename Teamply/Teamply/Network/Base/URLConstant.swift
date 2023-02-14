c45d0c9435218319630289d3c9f0795ecf196dc1
orzr <tjsdkfk77@naver.com>
2023-02-14 20:25:09 +0900

    [Network] 경로 추가 #58

--- Teamply/Teamply/Network/Base/URLConstant.swift
+++ Teamply/Teamply/Network/Base/URLConstant.swift
@@ -13,12 +13,13 @@ struct URLConstant {
     
     static let baseURL = "http://ec2-3-36-54-148.ap-northeast-2.compute.amazonaws.com"
     
-    static let signUp = "/user/signup"          //회원가입
-    static let login = "/user/login"            //로그인
+    static let signUp = "/user/signup"                              //회원가입
+    static let login = "/user/login"                                //로그인
     
-    static let userInto = "/user/my/account"    //개인 정보
-    static let userProject = "/project/my"      //개인 프로젝트
+    static let userInto = "/user/my/account"                        //개인 정보
+    static let userProject = "/project/my"                          //개인 프로젝트
     
-    static let createProject = "/project"       //프로젝트 생성
+    static let createProject = "/project"                           //프로젝트 생성
+    static let completedProject = "/project/my/finish/comments"     //완료된 팀 프로젝트
 
 }
