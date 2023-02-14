4b2d3465d67839ce8903f77f5ec63aab482e09c8
orzr <tjsdkfk77@naver.com>
2023-02-14 20:31:36 +0900

    [Network] 서비스 로직 구현 #58

---
+++ Teamply/Teamply/Network/Service/BoxService.swift
@@ -0,0 +1,41 @@
+//
+//  BoxService.swift
+//  Teamply
+//
+//  Created by 아라 on 2023/02/14.
+//
+
+import Moya
+
+enum BoxService {
+    case getCompletedProject
+    case getUserProject
+}
+
+extension BoxService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .getCompletedProject:
+            return URLConstant.completedProject
+        case .getUserProject:
+            return URLConstant.userProject
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getCompletedProject, .getUserProject:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
