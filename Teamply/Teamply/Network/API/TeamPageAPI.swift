bb376af61db03634dda654b1361c516ff2af7bfe
orzr <tjsdkfk77@naver.com>
2023-02-15 01:20:46 +0900

    [Network] 프로젝트 일정 API 구현 #59

---
+++ Teamply/Teamply/Network/API/TeamPageAPI.swift
@@ -0,0 +1,38 @@
+//
+//  TeamPageAPI.swift
+//  Teamply
+//
+//  Created by 아라 on 2023/02/15.
+//
+
+import Moya
+
+final class TeamPageAPI {
+    static let shared: TeamPageAPI = TeamPageAPI()
+    private let projectScheduleProvider = MoyaProvider<TeamPageService>(plugins: [MoyaLoggingPlugin()])
+    
+    private init() { }
+    
+    public private(set) var projectScheduleData: GeneralResponse<ProjectScheduleResponse>?
+    
+    // MARK: - Get
+    
+    func getProjectScheduleList(param: ProjectScheduleRequest, completion: @escaping ((GeneralResponse<ProjectScheduleResponse>?, Error?) -> ())) {
+        projectScheduleProvider.request(.getProjectSchedule(param: param)) { [weak self] response in
+            switch response {
+            case .success(let result):
+                do {
+                    self?.projectScheduleData = try result.map(GeneralResponse<ProjectScheduleResponse>?.self)
+                    guard let projectScheduleData = self?.projectScheduleData else { return }
+                    completion(projectScheduleData, nil)
+                } catch(let err) {
+                    print(err.localizedDescription)
+                    completion(nil, err)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil, err)
+            }
+        }
+    }
+}
