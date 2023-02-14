035a49b9e6bf97211221bf1ca5f899318191c6c3
orzr <tjsdkfk77@naver.com>
2023-02-15 01:14:59 +0900

    [Network] 프로젝트 일정 서비스 로직 구현 #59

---
+++ Teamply/Teamply/Network/Service/TeamPageService.swift
@@ -0,0 +1,38 @@
+//
+//  TeamPageService.swift
+//  Teamply
+//
+//  Created by 아라 on 2023/02/15.
+//
+
+import Moya
+
+enum TeamPageService {
+    case getProjectSchedule(param: ProjectScheduleRequest)
+}
+
+extension TeamPageService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .getProjectSchedule(let projectId):
+            return URLConstant.projectSchedule+"\(projectId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getProjectSchedule(_):
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
