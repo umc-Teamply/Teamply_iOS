96d99d6879dfd0813687b3ecae01b97322ead816
orzr <tjsdkfk77@naver.com>
2023-02-18 16:31:46 +0900

    [Network] 진행률 Response 모델에  추가 #59

--- Teamply/Teamply/Network/Model/TeamPageModel/ProjectScheduleResponse.swift
+++ Teamply/Teamply/Network/Model/TeamPageModel/ProjectScheduleResponse.swift
@@ -13,13 +13,14 @@ struct ProjectScheduleResponse: Codable {
 
 struct Schedule: Codable {
     let schID, projID: Int
-    let schTitle, schContents, startAt, endAt: String
+    let schTitle, schContents, schProgress, startAt, endAt: String
 
     enum CodingKeys: String, CodingKey {
         case schID = "sch_id"
         case projID = "proj_id"
         case schTitle = "sch_title"
         case schContents = "sch_contents"
+        case schProgress = "sch_progress"
         case startAt, endAt
     }
 }
