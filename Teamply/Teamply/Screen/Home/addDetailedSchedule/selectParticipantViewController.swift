4869eb4f23666be954316e5e414c981eef41826e
orzr <tjsdkfk77@naver.com>
2023-02-18 19:50:43 +0900

    [Fix] 프로젝트 세부페이지 수정 #74

--- Teamply/Teamply/Screen/Home/addDetailedSchedule/selectParticipantViewController.swift
+++ Teamply/Teamply/Screen/Home/addDetailedSchedule/selectParticipantViewController.swift
@@ -15,13 +15,11 @@ class selectParticipantViewController: UIViewController {
     @IBOutlet weak var user0Button: UIImageView!
     @IBOutlet weak var user2Button: UIImageView!
     @IBOutlet weak var user1Button: UIImageView!
-    @IBOutlet weak var user3Button: UIImageView!
     
     @IBOutlet weak var user0Label: UILabel!
     @IBOutlet weak var user2Label: UILabel!
     @IBOutlet weak var user1Label: UILabel!
-    @IBOutlet weak var user3Label: UILabel!
-    
+
     @IBOutlet weak var selectButton: UIButton!
     
     @IBOutlet weak var selectLabel: UILabel!
@@ -30,11 +28,15 @@ class selectParticipantViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         selectStyle()
-        user0Button.image = UIImage(named: "user0")
-        user2Button.image = UIImage(named: "user2")
-        user1Button.image = UIImage(named: "user1")
-        user3Button.image = UIImage(named: "user3")
-        
+        user0Button.image = UIImage(named: "duck")
+        user0Button.makeRound(radius: 31.5)
+        user0Button.isUserInteractionEnabled = true
+        user2Button.image = UIImage(named: "roseok")
+        user1Button.makeRound(radius: 31.5)
+        user1Button.isUserInteractionEnabled = true
+        user1Button.image = UIImage(named: "prince")
+        user2Button.makeRound(radius: 31.5)
+        user2Button.isUserInteractionEnabled = true
         
     }
     
@@ -43,45 +45,39 @@ class selectParticipantViewController: UIViewController {
         titleLabel.font = .sub2
         titleLabel.textColor = .basic2
         
-        user0Label.text = "이프로"
+        user0Label.text = "성덕선"
         user0Label.font = .cap2
         user0Label.textColor = .basic2
-        user0Button.layer.opacity = 0.3
+        //user0Button.layer.opacity = 0.3
         
-        user2Label.text = "박미룸"
+        user2Label.text = "장미옥"
         user2Label.font = .cap2
         user2Label.textColor = .basic2
-        user2Button.layer.opacity = 0.3
+        //user2Button.layer.opacity = 0.3
         
-        user1Label.text = "김늦음"
+        user1Label.text = "왕자현"
         user1Label.font = .cap2
         user1Label.textColor = .basic2
-        user1Button.layer.opacity = 0.3
-        
-        user3Label.text = "강꼼꼼"
-        user3Label.font = .cap2
-        user3Label.textColor = .basic2
-        user3Button.layer.opacity = 0.3
+        //user1Button.layer.opacity = 0.3
         
         selectButton.makeRound(radius: 10)
-        selectLabel.text = "선택 완료"
-        selectLabel.font = .btn
-        selectLabel.textColor = .basic1
+        selectButton.setTitle("선택완료", for: .normal)
+        selectButton.tintColor = .basic1
+        selectButton.setTitleColor(.basic1, for: .normal)
+        selectButton.titleLabel!.font = .btn
+        
     }
     
     @IBAction func tapUser0Button(_ sender: Any) {
-        user0Button.layer.opacity = 1.0
+        user0Button.layer.opacity = 0.5
     }
     
     @IBAction func tapUser2Button(_ sender: Any) {
-        user2Button.layer.opacity = 1.0
+        user2Button.layer.opacity = 0.5
     }
     
     @IBAction func tapUser1Button(_ sender: Any) {
-        user1Button.layer.opacity = 1.0
+        user1Button.layer.opacity = 0.5
     }
     
-    @IBAction func tapUser3Button(_ sender: Any) {
-        user3Button.layer.opacity = 1.0
-    }
 }
